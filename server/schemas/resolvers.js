65232aa8f1a24502931ec6eab313be42f56396aa
Andrew Midthun <midth002@gmail.com>
2022-06-23 10:32:14 -0500

    added login mutation to grab token and thow authentication errors if credentials fail from the user.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -72,7 +72,36 @@ const resolvers = {
             }
     
             // throw new AuthenticationError('Not logged in');
-     }
+        },
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, {new: true});
+            }
+
+            // throw new AuthenticationError('Not logged in');
+        },
+        updateProduct: async (parent, { _id, quantity }) => {
+            const decrement = Math.abs(quantity) * -1;
+
+            return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement}}, { new: true });
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                  throw new AuthenticationError('User not found.');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if(!correctPassword) {
+                throw new AuthenticationError('Incorrect password given.');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        } 
 
      
 
