2dead249bc4762779271f6689d3eed5a02582ef6
Andrew Midthun <midth002@gmail.com>
2022-06-23 12:34:18 -0500

    Added to login page. No styling yet.

---
+++ client/src/pages/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+
+    const [formState, setFormState] = useState({email: '', password: ''});
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password }
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+  return (
+    <div>
+        <h2>Login</h2>
+        <form onSubmit={handleFormSubmit}>
+            <div>
+                <label htmlFor='email'>Email address: </label>
+                <input 
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='pwd'>Password</label>
+                <input 
+                placeholder='********'
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+                />
+            </div>
+        
+            {error ? (
+                <div>
+                    <p>The provided credentials are incorrect</p>
+                </div>
+            ) : null}
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+            <div>
+                <Link to="/signup">Signup Instead</Link>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
