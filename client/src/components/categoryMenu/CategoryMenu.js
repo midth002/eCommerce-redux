0b813e6fefe36f4b3925ce383d5704da80315225
Andrew Midthun <midth002@gmail.com>
2022-06-23 19:09:58 -0500

    Added category menu to load different types of categories.

--- client/src/components/categoryMenu/CategoryMenu.js
+++ client/src/components/categoryMenu/CategoryMenu.js
@@ -6,4 +6,55 @@ import {
     UPDATE_CURRENT_CATEGORY
 } from '../../utils/actions';
 import { QUERY_CATEGORES } from '../../utils/queries';
-import { idbPromis } from '../../utils/helpers';
+import { idbPromise } from '../../utils/helpers';
+
+
+const CategoryMenu = () => {
+    const [state, dispatch] = useStoreContext();
+
+    const { categories } = state;
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORES);
+
+    useEffect(() => {
+        if (categoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories,
+            });
+            categoryData.categories.forEach((category) => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then((categories) => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories
+                });
+            });
+        }
+    }, [categoryData, loading, dispatch]);
+
+    const handleClick = (id) => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id,
+        });
+    };
+
+  return (
+    <div>
+        <h2>Choose a Category: </h2>
+        {categories.map((item) => (
+            <button 
+            key={item._id}
+            onClick={() => {
+                handleClick(item._id);
+            }}
+            >{item.name}</button>
+        ))
+        }
+    </div>
+  )
+}
+
+export default CategoryMenu
